c0b2d9e036acf0af3680f8f0a83d35204f2ee0b5
ponjae <>
2021-04-19 13:48:39 +0200

    Logic for the second part of the game should now be implemented

--- Roll.py
+++ Roll.py
@@ -3,13 +3,26 @@ class Roll:
         pass
 
     def check_one_pair(self, dice_list):
-        pass
+        for item in dice_list:
+            if dice_list.count(item) > 1:
+                return True
+        return False
 
     def check_two_pair(self, dice_list):
-        pass
+        dice_list.sort()
+        # xx0yy
+        first = dice_list[0] == dice_list[1] and dice_list[-2] == dice_list[-1]
+        # 0xxyy
+        second = dice_list[1] == dice_list[2] and dice_list[-2] == dice_list[-1]
+        # xxyy0
+        third = dice_list[0] == dice_list[1] and dice_list[2] == dice_list[3]
+        return first or second or third
 
     def check_three_kind(self, dice_list):
-        pass
+        dice_list.sort()
+        if len(set(dice_list)) > 3:
+            return False
+        return dice_list.count(dice_list[2]) >= 3
 
     def check_four_kind(self, dice_list):
         dice_list.sort()
@@ -19,11 +32,11 @@ class Roll:
 
     def check_low_straight(self, dice_list):
         dice_list.sort()
-        return len(set(dice_list)) == 5 and dice_list[0] == 1
+        return len(set(dice_list)) == 5 and dice_list[0] == 1 and dice_list[-1] == 5
 
     def check_high_straight(self, dice_list):
         dice_list.sort()
-        return len(set(dice_list)) == 5 and dice_list[0] == 2
+        return len(set(dice_list)) == 5 and dice_list[0] == 2 and dice_list[-1] == 6
 
     def check_full_house(self, dice_list):
         dice_list.sort()
@@ -43,8 +56,3 @@ class Roll:
         if len(set(dice_list)) == 1:
             return True
         return False
-
-
-roller = Roll()
-
-print(roller.check_four_kind([1, 1, 1, 2, 2]))
