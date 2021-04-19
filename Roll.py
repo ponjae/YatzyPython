799b57ca88a5ceb9e76e81122b01d7b85192a074
ponjae <>
2021-04-19 12:38:11 +0200

    initial commit

---
+++ Roll.py
@@ -0,0 +1,15 @@
+class Roll:
+    def __init__(self):
+        pass
+
+    def check_one_pair(self, dice_list):
+        pass
+
+    def check_two_pair(self, dice_list):
+        pass
+
+    def check_three_kind(self, dice_list):
+        pass
+
+    def check_four_kind(self, dice_list):
+        pass
